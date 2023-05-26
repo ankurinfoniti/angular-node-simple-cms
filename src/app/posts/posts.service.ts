8992ce14c22a4eed84646dc1fa4db08c234878ac
ankurinfoniti <ankur.infoniti@gmail.com>
2023-05-26 13:58:27 +0530

    connect post api in client side

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -29,8 +29,12 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: 'aaaaa', title, content };
-    this.posts.push(post);
-
-    this.postsUpdated.next([...this.posts]);
+    this.httpClient
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((responsDate) => {
+        console.log(responsDate);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
