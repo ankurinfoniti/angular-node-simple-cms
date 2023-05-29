9e34444284c066eff0716dfcf2959a783401b74a
ankurinfoniti <ankur.infoniti@gmail.com>
2023-05-29 14:49:59 +0530

    create user on signup

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", async (req, res, next) => {
+  const hash = await bcrypt.hash(req.body.password, 10);
+
+  const user = new User({
+    email: req.body.email,
+    password: hash,
+  });
+
+  const result = await user.save();
+
+  res.status(201).json({
+    message: "User created successfully",
+    result: result,
+  });
+});
 
 module.exports = router;
