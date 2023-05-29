370cd3a4709def83b66c3030dea02787239d9d69
ankurinfoniti <ankur.infoniti@gmail.com>
2023-05-29 13:04:37 +0530

    add form submit method

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -23,4 +23,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 })
 export class LoginComponent {
   isLoading = false;
+
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
 }
